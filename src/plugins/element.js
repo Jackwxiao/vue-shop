7540da853732b8891193d16b3eae16c588bd0877
wuxiao <1291383707@qq.com>
2020-02-11 23:20:45 +0800

    完成订单页面

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -5,7 +5,7 @@ import {
     MenuItemGroup, MenuItem, Breadcrumb, BreadcrumbItem,
     Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog,
     MessageBox, Tag, Tree, Select, Option, Cascader, Alert, Tabs, TabPane,
-    Step, Steps, CheckboxGroup, Checkbox, Upload
+    Step, Steps, CheckboxGroup, Checkbox, Upload, Timeline, TimelineItem
 } from 'element-ui'
 
 Vue.use(Button)
@@ -44,5 +44,7 @@ Vue.use(Step)
 Vue.use(CheckboxGroup)
 Vue.use(Checkbox)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
