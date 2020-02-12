327bf6e9b8ed56fedd1ae721dc4cea6dd43bc8e0
wuxiao <1291383707@qq.com>
2020-02-12 15:32:05 +0800

    添加报表页面

---
+++ src/components/report/report.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+        111
+    </el-card>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  },
+  created() {},
+  methods: {}
+}
+</script>
+<style lang="less" scoped>
+</style>
