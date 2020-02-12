e225b6fadc3eb60ef7a4783e322587e3d0449a40
wuxiao <1291383707@qq.com>
2020-02-12 18:09:34 +0800

    优化: 自定制首页的渲染

--- vue.config.js
+++ vue.config.js
@@ -5,20 +5,30 @@ module.exports = {
             config.entry('app').clear().add('./src/main-prod.js')
 
             //使用externals设置排除项
-            config.set('externals',{
-                vue:'Vue',
-                'vue-router':'VueRouter',
-                axios:'axios',
-                lodash:'_',
-                echarts:'echarts',
-                nprogress:'NProgress',
-                'vue-quill-editor':'VueQuillEditor'
+            config.set('externals', {
+                vue: 'Vue',
+                'vue-router': 'VueRouter',
+                axios: 'axios',
+                lodash: '_',
+                echarts: 'echarts',
+                nprogress: 'NProgress',
+                'vue-quill-editor': 'VueQuillEditor'
+            })
+
+            config.plugin('html').tap(args => {
+                args[0].isProd = true
+                return args
             })
         })
 
         // 开发模式
         config.when(process.env.NODE_ENV === 'development', config => {
             config.entry('app').clear().add('./src/main-dev.js')
+
+            config.plugin('html').tap(args => {
+                args[0].isProd = false
+                return args
+            })
         })
     }
 }
