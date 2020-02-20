8bb91aeeec93ee5372239f0ea629e9de034abbbe
wuxiao <1291383707@qq.com>
2020-02-20 15:59:06 +0800

    修改部署路径

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/vue-shop/dist/'
+        ? '/shop-online/dist/'
         : '/'
 ,
     chainWebpack: config => {
